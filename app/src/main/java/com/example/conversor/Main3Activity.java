8ee243569c02dbea014b4f3ac134566dca088b65
Henrique Xavier <henriquexavierb@gmail.com>
2019-04-24 23:33:00 -0300

    Update Main3Activity.java

--- app/src/main/java/com/example/conversor/Main3Activity.java
+++ app/src/main/java/com/example/conversor/Main3Activity.java
@@ -7,13 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
 
     private EditText caixaTexto;
     private Button botaoConverter;
     private TextView resultado;
-    private TextView mensagemError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +23,6 @@ public class Main3Activity extends AppCompatActivity {
         caixaTexto = (EditText) findViewById(R.id.caixaTextold);
         botaoConverter = (Button) findViewById(R.id.botaolConverterld);
         resultado = (TextView) findViewById(R.id.resultadoId);
-        mensagemError = (TextView) findViewById(R.id.mensagemErrorId);
 
         botaoConverter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +34,7 @@ public class Main3Activity extends AppCompatActivity {
                 double valorResultadoFareheint;
 
                 if (textoDigitado.isEmpty()) {
-                    mensagemError.setText("Nenhum numero digitado!!!");
+                    Toast.makeText(getApplicationContext(), "Digite um valor!", Toast.LENGTH_SHORT).show();
 
                 } else {
                     if (textoDigitado.startsWith("-")) {
@@ -47,7 +46,6 @@ public class Main3Activity extends AppCompatActivity {
                     }
                     valorResultadoCelsius = valorDigitado - 273;
                     valorResultadoFareheint = (valorDigitado - 273) * 9/5 + 32;
-                    mensagemError.setText("");
                     resultado.setText("A temperatura em celsius é de:\n" + valorResultadoCelsius + "C" + "\nA temperatura em Fahrenheint é de:\n" + valorResultadoFareheint + "F");
 
                 }
@@ -64,6 +62,11 @@ public class Main3Activity extends AppCompatActivity {
                 finish();
             }
         });
-
+    }
+    @Override
+    public void onBackPressed() {
+        Intent TELA1 = new Intent(Main3Activity.this, MainActivity.class);
+        startActivity(TELA1);
+        super.onBackPressed();
     }
 }
